b9802b2012d73966b313980763e24479c15837dd
Z User <z@container>
2026-06-28 16:13:20 +0000

    960c53ff-9e66-4b55-97e6-688d00f8b8c7

--- start-server.sh
+++ start-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/z/my-project
 while true; do
-    php -S 127.0.0.1:3000 -t public server.php 2>>/tmp/php-server.log
+    /usr/local/bin/php -S 0.0.0.0:3000 -t public server.php 2>>/home/z/my-project/dev.log
     sleep 1
 done
